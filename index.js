cd9b65496c4aa7715f01b1c7b11cf1c3bbf53da3
Martin Renteria <mrrenteria0159@gmail.com>
2023-11-25 15:03:16 -0700

    initial commit, add questions to index

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateShapes = require('./lib/generateShapes')
+
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Text: Enter up to three(3) characters',
+    },
+    {
+        type: 'input',
+        name: 'text-color',
+        message: 'Text-color: Enter a color keyword or hexadecimal number',
+    },
+    {
+        type: 'input',
+        name: 'shape',
+        message: 'Shape Color: Enter a color keyword or hexadecimal number',
+    },
+    {
+        type: 'checkbox',
+        name: 'image',
+        message: 'Select which image you would like.',
+        choices: ['Circle,', 'Square', 'Triangle'],
+    },
+];
+function writeToFile(fileName, data) {
+   return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
+const init = () => {
+    inquirer.prompt(questions)
+    .then((responses)=> {
+        console.log('Generating SVG file')
+        writeToFile('./SVG.png', generateShapes)
+    })
+}
