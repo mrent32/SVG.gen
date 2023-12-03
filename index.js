f6e1047744c40870318152c38896ace1238c5a0f
Martin Renteria <mrrenteria0159@gmail.com>
2023-12-03 13:14:27 -0700

    app now generates the shape

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import inquirer from "inquirer";
-import fs from "fs";
-import Shape from "./lib/generateShapes.js";
+const { prompt} = require('inquirer')
+const fs = require('fs')
+const { Circle, Square, Triangle }  = require('./lib/Shapes')
+const path = require('path')
 
 
 const questions = [
@@ -11,29 +12,49 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'text-color',
+        name: 'textColor',
         message: 'Text-color: Enter a color keyword or hexadecimal number',
     },
     {
         type: 'input',
-        name: 'shape',
+        name: 'shapeColor',
         message: 'Shape Color: Enter a color keyword or hexadecimal number',
     },
     {
-        type: 'checkbox',
+        type: 'list',
         name: 'image',
         message: 'Select which image you would like.',
-        choices: ['Circle,', 'Square', 'Triangle'],
+        choices: ['Circle', 'Square', 'Triangle'],
     },
+    // {
+    //     type: 'input',
+    //     name: 'filename',
+    //     message: 'Give your logo a name.',
+    // },
 ];
 function writeToFile(fileName, data) {
-   return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+   return fs.writeFileSync(path.join(process.cwd(), "/Examples/", fileName), data)
 }
 
 const init = () => {
-    inquirer.prompt(questions)
+    prompt(questions)
     .then((responses)=> {
-        console.log('Generating SVG file')
-        writeToFile('./SVG.png', Shape)
+        console.log('Generating SVG file', responses)
+        let svgString = ''
+        if (responses.image === "Circle") {
+            const circle = new Circle(responses.text, responses.textColor, responses.shapeColor)
+            svgString = circle.renderStart() + circle.renderShape() + circle.renderText() + circle.renderEnd()
+        } else if (responses.image === "Square") {
+            const square = new Square(responses.text, responses.textColor, responses.shapeColor)
+            svgString = square.renderStart() + square.renderShape() + square.renderText() + square.renderEnd();
+            console.log(svgString)
+        } else {
+            const triangle = new Triangle(responses.text, responses.textColor, responses.shapeColor)
+            svgString = triangle.renderStart() + triangle.renderShape() + triangle.renderText() + triangle.renderEnd();
+            console.log(svgString)
+        }
+        writeToFile('logo.svg', svgString)
+
     })    
 }
+init();
